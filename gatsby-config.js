cbb2e783bd63a7ab287ac3d0d8af2ef6478c595a
Guilherme Barros <hello@gbarros.dev>
2021-01-21 16:30:50 -0300

    SocialLinks and MenuLinks

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
+        icon: `${__dirname}/src/icons`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,16 @@
+const links = [
+  {
+    label: "Github",
+    url: "https://github.com/gbarros95",
+  },
+  {
+    label: "Twitter",
+    url: "https://twitter.com/gui_bbarros",
+  },
+  {
+    label: "Instagram",
+    url: "https://www.instagram.com/guilherme_bbarros",
+  },
+]
+
+export default links
